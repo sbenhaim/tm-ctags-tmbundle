b319354f51a41e1c38a82b4ae8212119e786f44d
Selah <selah@clockwork.net>
2008-07-17 17:36:24 -0500

    Docs cleaned up, stuff renamed, other stuff

--- Support/bin/tmctags.rb
+++ Support/bin/tmctags.rb
@@ -3,7 +3,8 @@
 require ENV['TM_SUPPORT_PATH'] + '/lib/ui.rb'
 require ENV['TM_SUPPORT_PATH'] + '/lib/exit_codes.rb'
 require ENV['TM_SUPPORT_PATH'] + '/lib/textmate.rb'
-require ENV['TM_BUNDLE_SUPPORT'] + '/lib/ctags.rb'
+
+require ENV['TM_BUNDLE_SUPPORT'] + '/lib/tm_ctags.rb'
 
 unless ENV['TM_PROJECT_DIRECTORY']
   TextMate.exit_show_tool_tip "You must be working with a project or directory to use TM_Ctags."
@@ -15,57 +16,59 @@ unless File.exists?( ENV['TM_PROJECT_DIRECTORY'] + "/.tmtags" )
   exit
 end
 
-def perform(action)
+RESULT_LIMIT = 300
 
-  case action
-  when 'complete'
-    nib = 'AutoComplete'
-    word = ENV['TM_CURRENT_WORD']
-    regex = /^#{word}/i
-    method = TM_Ctags.method(:build_snippet)
-  when 'find'
-    nib = 'GotoSymbol'
-    word = ENV['TM_CURRENT_WORD']
-    regex = /^#{word}[^\w]/
-    method = TM_Ctags.method(:tm_goto)
-  when 'goto'
-    nib = 'GotoSymbol'
-    word = TextMate::UI.request_string :title => "Goto Project Symbol", :prompt => "Symbol"
-    regex = /^#{word}/i
-    method = TM_Ctags.method(:tm_goto)
+action = ARGV[0]
+
+case action
+when 'complete'
+  nib = 'AutoComplete'
+  word = ENV['TM_CURRENT_WORD']
+  regex = /^#{word}/i
+  method = TM_Ctags.method(:build_snippet)
+when 'find'
+  nib = 'GotoSymbol'
+  word = ENV['TM_CURRENT_WORD']
+  regex = /^#{word}[^\w]/
+  method = TM_Ctags.method(:goto)
+when 'goto'
+  nib = 'GotoSymbol'
+  word = TextMate::UI.request_string :title => "Goto Project Symbol", :prompt => "Symbol"
+  regex = /^#{word}/i
+  method = TM_Ctags.method(:goto)
+else
+  exit
+end
+
+nib = ENV['TM_BUNDLE_SUPPORT'] + "/nibs/#{nib}.nib"
+
+tags = File.read( ENV['TM_PROJECT_DIRECTORY'] + "/.tmtags" ).grep( regex )
+
+hits = []
+
+index = 1
+tags.each do |line| 
+  hit = TM_Ctags::parse(line, index) 
+  unless hit['type'] == 'variable'
+    hits << hit
+    index += 1
   end
+  break if index > RESULT_LIMIT;
+end
 
-  nib = ENV['TM_BUNDLE_SUPPORT'] + "/nibs/#{nib}.nib"
+TextMate.exit_show_tool_tip "not found" if hits.length == 0
 
-  tags = File.read( ENV['TM_PROJECT_DIRECTORY'] + "/.tmtags" ).grep( regex )
+if hits.length < 2
+	print method.call(hits[0] )
+  exit
+end
 
-  hits = []
+result = %x{ "$DIALOG" -mc -p '#{{'hits' => hits}.to_plist.gsub("'", '"')}' "#{nib}" | pl }
 
-  index = 1
-  tags.each do |line| 
-    hit = TM_Ctags::parse(line, index) 
-    unless hit['type'] == 'variable'
-      hits << hit
-      index += 1
-    end
-    break if index > 300;
-  end
+result = OSX::PropertyList.load(result)
 
-  TextMate.exit_show_tool_tip "not found" if hits.length == 0
-
-  if hits.length < 2
-  	print method.call(hits[0] )
-    exit
-  end
-
-  result = %x{ "$DIALOG" -mc -p '#{{'hits' => hits}.to_plist.gsub("'", '"')}' "#{nib}" | pl }
-
-  result = OSX::PropertyList.load(result)
-
-  if result['result']
-    print method.call( result['result']['returnArgument'][0] )
-  else
-    TextMate.exit_discard
-  end
-  
+if result['result']
+  print method.call( result['result']['returnArgument'][0] )
+else
+  TextMate.exit_discard
 end
--- Support/docs/help.markdown
+++ Support/docs/help.markdown
@@ -3,9 +3,9 @@ Update Tags
 
 ### ⌥⌘P
 
-Builds a .tmtags file in the root directory of your project. This is a prerequisite to running any of the other commands in this bundle, and should be run as your project changes.
+Builds a .tmtags file in the root directory of your project. This is a prerequisite to running any of the other commands in this bundle and should be run as your project changes.
 
-This (and all other commands) will only work with a project or directory open in TextMate.
+This (and all other commands in this bundle) will only work with a project or directory open in TextMate.
 
 Goto Current Tag
 ----------------
@@ -14,16 +14,16 @@ Goto Current Tag
 
 Jumps to the file and line where the tag under the cursor is declared.
 
-Project Autocomplete
+Project Completion
 --------------------
 
 ### ⌘<
 
-Start typing a tag name, Project Autocomplete will insert a completion including method/function arguments.
+Provides completions for the current word&mdash;including method/function arguments&mdash;pulling from the functions, methods, and classes declared in your project.
 
 Goto Tag...
 -----------
 
 ### ⌘⇧P
 
-Prompts for a tag (or the start of a tag) and goes the the file and line where that tag is declared.
+Prompts for a tag (or the start of a tag) and jumps to the file and line where that tag is declared.
