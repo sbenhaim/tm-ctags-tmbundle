68a6a3d75c414c26f27d097a4446e75d81072159
Selah <selah@clockwork.net>
2008-07-21 09:58:54 -0500

    Fixed uuid conflict; using TM's progress lib

--- Support/bin/update_ctags.rb
+++ Support/bin/update_ctags.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require ENV['TM_SUPPORT_PATH'] + '/lib/textmate.rb'
-require ENV['TM_SUPPORT_PATH'] + '/lib/ui.rb'
+require ENV['TM_SUPPORT_PATH'] + '/lib/progress.rb'
 
 dir = ENV['TM_PROJECT_DIRECTORY']
 nib = ENV['TM_BUNDLE_SUPPORT'] + "/nibs/Progress.nib"
@@ -23,9 +23,6 @@ ctags_bin = ENV['TM_BUNDLE_SUPPORT'] + '/bin/ctags'
   
 Dir.chdir(dir)
 
-working = { 'working' => 1, 'text' => "Indexing your project..." }
-token = %x{"$DIALOG" -ac -p '#{working.to_plist}' "#{nib}" | pl}
-token = OSX::PropertyList.load(token)
-result = `"#{ctags_bin}" #{args.join(' ')}`
-working = { 'working' => 0, 'text' => 'All done.'}
-%x{"$DIALOG" -t #{token} -p '#{working.to_plist}'}
+TextMate.call_with_progress( :title => "TM Ctags", :message => "Indexing your project…", :indeterminate => true ) do
+  result = `"#{ctags_bin}" #{args.join(' ')}`
+end
